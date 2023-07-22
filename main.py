e6f681ed05726fe4a99f59cd4eedb640ac121b67
Med Saada <aminousaadagmail.com>
2023-07-22 22:12:56 +0100

    getting the distance between the up finger and the down finger

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2 as cv
 from cvzone.HandTrackingModule import HandDetector
 import numpy
 import math
+from time import  sleep
+
 # from utilities import *
 
 #instantating the detector object
@@ -18,11 +20,30 @@ cap =cv.VideoCapture(0)
 while True:
     ret,frame=cap.read()
     hands, img = ObjetHandDetector.findHands(frame)
-    print(hands)
+    # try :
+    #     print(hands[8])
+    # except :
+    #     print("nothing now")
+    if len(hands):
+        listPositions = hands[0]
+        points = listPositions["lmList"]
+        UpFinger = points[8]
+        DownFinger = points[4]
+        UpFingerShrink = UpFinger[0:2]
+        DownFingerShrink=DownFinger[0:2]
+        projections=[]
+        for a, b in zip(UpFingerShrink, DownFingerShrink):
+            projections.append(a - b)
+        distance=pow(projections[0],2)+pow(projections[1],2)
+        print(distance)
+        cv.circle(frame,tuple(UpFingerShrink),radius=5,color=(255,0,0),thickness=-1)
+        cv.circle(frame, tuple(DownFingerShrink), radius=5, color=(255, 0, 0), thickness=-1)
 
-    cv.imshow("Output",frame)
 
-    if cv.waitKey(1)&0xFF==ord('q'):
+
+
+    cv.imshow("Output", frame)
+    if cv.waitKey(10)&0xFF==ord('q'):
         break
 cap.release()
 cv.destroyAllWindows()
